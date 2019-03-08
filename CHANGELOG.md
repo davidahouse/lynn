4da45e7950adfd78c161221777d7c20aa8500b83
David House <david.house@kp.org>
2019-03-08 10:35:50 -0500

    :sparkles: Changed capture to handle array and non array better

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## lynn
 
+## 0.2.0
+
+- [5] Default to non-array when capturing, but you can capture an array by surrounding path with []
+
 ## 0.1.0
 
 - Initial release
--- test/lynn-request-tests.js
+++ test/lynn-request-tests.js
@@ -1,6 +1,9 @@
 /* eslint-env mocha */
 const expect = require('chai').expect
 const LynnRunner = require('../index.js')
+const nock = require('nock')
+
+const successResponseBasic = require('../test/mockResponses/200_basic')
 
 const successAPIRequest = {
   'options': {
@@ -8,26 +11,72 @@ const successAPIRequest = {
     'method': 'GET',
     'host': '${this.HOST}',
     'port': '${this.PORT}',
-    'path': '/contents/',
+    'path': '/contents',
+  },
+  'capture': {
+    'NAME': '$.data.name',
+    'NAMEASARRAY': '[$.data.name]',
+    'NOTFOUND': '$.data.notfound',
   },
 }
 
 const environment = {
-  'HOST': '192.168.1.25',
+  'HOST': 'localhost',
   'PORT': 8080,
 }
 
+/**
+ * nockSuccessBasic
+ * @return {object} scope
+ */
+function nockSuccessBasic() {
+  nock.cleanAll()
+  const scope = nock('http://localhost:8080')
+      .get('/contents')
+      .reply(200, successResponseBasic)
+  return scope
+}
+
 describe('Lynn Runner', function() {
+  beforeEach(() => {
+  })
+
   it('should retain request after construction', function() {
     const runner = new LynnRunner(successAPIRequest, environment)
     expect(runner.request.options.method).to.equal('GET')
   })
 
   describe('execute', function() {
-    it('should call the callback when finished', function() {
+    it('should call the callback when finished', function(done) {
+      nockSuccessBasic()
       const runner = new LynnRunner(successAPIRequest, environment)
       runner.execute(function(result) {
         expect(result.statusCode).to.equal(200)
+        done()
+      })
+    })
+  })
+
+  describe('capture', function() {
+    it('should capture a value from the result data', function(done) {
+      nockSuccessBasic()
+      const runner = new LynnRunner(successAPIRequest, environment)
+      runner.execute(function(result) {
+        expect(result.statusCode).to.equal(200)
+        const captured = runner.captured(result)
+        expect(captured['NAME']).to.deep.equal('Fred')
+        expect(captured['NAMEASARRAY']).to.deep.equal(['Fred'])
+        done()
+      })
+    })
+
+    it('should handle capture paths that are not found', function(done) {
+      nockSuccessBasic()
+      const runner = new LynnRunner(successAPIRequest, environment)
+      runner.execute(function(result) {
+        const captured = runner.captured(result)
+        expect(captured.hasOwnProperty('NOTFOUND')).to.equal(false)
+        done()
       })
     })
   })
